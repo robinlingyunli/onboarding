aedb067cc77a30d877f7991d62966cd76d5d6b20
robinlingyunli <lil4@rose-hulman.edu>
2020-10-05 18:22:17 -0400

    map not working

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,13 +1,18 @@
-import React, { FC, Fragment, useEffect, useState } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { Accordion, Button, Card } from 'react-bootstrap';
+import './index.scss';
 
 interface Tweet {
   text: string;
   videoPath?: string;
 }
 
-const App: FC = () => {
-  const [tweets, setTweets] = useState<Tweet[]>();
+interface TweetContainerProps {
+  tweets: Tweet[];
+}
 
+const App: FC = (props) => {
+  const [tweets, setTweets] = useState<Tweet[]>();
   useEffect((): void => {
     (async () => {
       let fetchedTweets: Tweet[];
@@ -21,12 +26,45 @@ const App: FC = () => {
     })().catch(console.error);
   }, []);
 
-  return (
-    <Fragment>
-      <div>Hello world!</div>
-      {tweets && <pre>{JSON.stringify(tweets, null, 4)}</pre>}
-    </Fragment>
-  );
+  const TweetContainer: FC<TweetContainerProps> = (props) => {
+    let i = -1;
+    return (
+      <div>
+        {props.tweetList.map((tweet: Tweet) => {
+          return <TweetBox key={(i += 1)} tweet={tweet} />;
+        })}
+      </div>
+    );
+  };
+
+  const TweetBox: FC = (props) => {
+    console.log(props.tweet);
+    return (
+      <div>
+        <Accordion defaultActiveKey="0">
+          <Card>
+            <Accordion.Toggle as={Button} variant="link" eventKey="0">
+              Tweet here
+            </Accordion.Toggle>
+            <Accordion.Collapse eventKey="0">
+              <Card.Body>Hi</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+          <Card>
+            <Card.Header>
+              <Accordion.Toggle as={Button} variant="link" eventKey="1">
+                Video Address here
+              </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey="1">
+              <Card.Body>Hello! I am another body</Card.Body>
+            </Accordion.Collapse>
+          </Card>
+        </Accordion>
+      </div>
+    );
+  };
+  return <TweetContainer tweetList={tweets} />;
 };
 
 export default App;
