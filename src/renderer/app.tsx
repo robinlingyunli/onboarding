5e50a98e090d92c7e73f9b88ab62b5099d5779dd
robinlingyunli <lil4@rose-hulman.edu>
2020-10-13 21:03:05 -0400

    all done

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -13,12 +13,19 @@ interface TweetContainerProps {
 
 interface TweetBoxProps {
   tweet: Tweet;
+  id: string;
 }
+
+let finalTweets = [];
+let setColor = 'black';
+let bgColor = '#F8EFCE';
 
 const App: FC = (props) => {
   const init = [];
   const [tweets, setTweets] = useState<Tweet[]>(init);
+  console.log('app started');
   useEffect((): void => {
+    console.log('useEffect called');
     (async () => {
       let fetchedTweets: Tweet[];
       do {
@@ -26,17 +33,42 @@ const App: FC = (props) => {
           (response) => response.json() as Promise<Tweet[]>,
         );
       } while (!fetchedTweets.length);
-      setTweets(fetchedTweets);
-      // eslint-disable-next-line no-console
+      finalTweets = fetchedTweets;
+      // console.log(tweets);
+      setInterval(() => {
+        addOnTop();
+        console.log('setInterval called');
+      }, 15000);
     })().catch(console.error);
   }, []);
+  // useEffect((): void => {
+  //   setTweets(finalTweets);
+  //   console.log('useeffect called');
+  // }, [tweets]);
+  function addOnTop() {
+    (async () => {
+      let fetchedTweets: Tweet[];
+      do {
+        fetchedTweets = await fetch('https://r-t-generator.herokuapp.com/').then(
+          (response) => response.json() as Promise<Tweet[]>,
+        );
+      } while (!fetchedTweets.length);
+      finalTweets.unshift(fetchedTweets[0]);
+      console.log('setValue is: ');
+      setTweets([...finalTweets]);
+    })().catch(console.error);
+  }
+
+  console.log('tweet is: ');
+  console.log(tweets);
 
   const TweetContainer: FC<TweetContainerProps> = (props) => {
+    // console.log(tweets)；
     let i = -1;
     return (
       <div>
         {props.tweets.map((tweet: Tweet) => {
-          return <TweetBox key={(i += 1)} tweet={tweet} />;
+          return <TweetBox key={(i += 1)} tweet={tweet} id={i.toString()} />;
         })}
       </div>
     );
@@ -44,25 +76,26 @@ const App: FC = (props) => {
 
   const TweetBox: FC<TweetBoxProps> = (props) => {
     const tweetWord = props.tweet.text.split(' ');
-
     let wordShow = [];
     for (let i = 0; i < 15; i++) {
       if (i == tweetWord.length - 1) break;
       wordShow += tweetWord[i] + ' ';
     }
-    console.log(wordShow);
+
     const videoPath = 'https://r-t-generator.herokuapp.com/';
     return (
       <div>
-        <Accordion defaultActiveKey="0">
-          <Card>
-            <Accordion.Toggle as={Button} variant="link" eventKey="0">
-              {wordShow}
+        <Accordion>
+          <Card style={{ background: bgColor }}>
+            <Accordion.Toggle as={Button} style={{ background: bgColor }} eventKey="0">
+              <p style={{ color: setColor }}>{wordShow}</p>
             </Accordion.Toggle>
             <Accordion.Collapse eventKey="0">
               <Card.Body>
-                <div>{props.tweet.text}</div>
                 <div>
+                  <p style={{ color: setColor }}>{props.tweet.text}</p>
+                  <br></br>
+
                   <video width="300" height="200" src={videoPath + props.tweet.videoPath} controls>
                     Video not supported
                   </video>
@@ -74,7 +107,30 @@ const App: FC = (props) => {
       </div>
     );
   };
-  return <TweetContainer tweets={tweets} />;
+
+  function clickHandlerToDark() {
+    setColor = '#FFF8EE';
+    bgColor = '#A9A9A9';
+    setTweets([...finalTweets]);
+  }
+
+  function clickHandlerToLight() {
+    setColor = 'Black';
+    bgColor = '#F8EFCE';
+    setTweets([...finalTweets]);
+  }
+
+  return (
+    <div align="center">
+      <div>
+        <button onClick={clickHandlerToDark}>Click to change color theme to dark!</button>
+        <button onClick={clickHandlerToLight}>Click to change color theme to light!</button>
+      </div>
+      <div>
+        <TweetContainer tweets={tweets} />
+      </div>
+    </div>
+  );
 };
 
 export default App;
